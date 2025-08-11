3205f15bb1101b18fda7efc2b91a8c0887a9e64a
alduin0 <alduin@posteo.de>
2025-08-11 11:03:21 +0200

    pulling data from yf and splitting into train, val, test

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from src.func_create_dataset import create_dataset as CD
+from src.func_create_dataset import create_dataset as CDS
 import json
 import pathlib as pl
 import logging
+import inspect
 import datetime
 
-timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+#timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 logging.basicConfig(level=logging.DEBUG,
-                    filename=pl.Path.cwd().joinpath('logs', f"mainlog-{timestamp}.log"),
+                    filename=pl.Path.cwd().joinpath('logs', f"mainlog-{420}.log"),
                     encoding='utf-8',
-                    format='%(asctime)s - PID %(process)d - %(name)s - %(levelname)s - %(message)s',)
-mainlog = logging.getLogger(__name__)
+                    format='%(asctime)s - PID %(process)d - %(name)s - %(levelname)s - %(message)s',
+                    filemode='w')
+mylog = logging.getLogger(__name__)
 
 def main():
+    mylog.info(f"Called function: {inspect.stack()[0].function}")
     # reading in configuration information from json file ----------------------------------
     path_config = pl.Path.cwd()/ 'input' / 'config.json'
     try:
         config = json.load(open(path_config))
-        mainlog.info(f"Succesfully loaded config from: {path_config}")
-        mainlog.info(f"The config file is: {config}")
+        mylog.info(f"Succesfully loaded config from: {path_config}")
+        mylog.info(f"The config file is: {config}")
     except FileNotFoundError:
-        mainlog.error(f"Config file not found: {path_config}")
+        mylog.error(f"Config file not found: {path_config}")
         raise FileNotFoundError('config.json not found, check config path')
 
-
+    # constructing pytorch dataset from tickers and other ----------------------------------
+    # parameters passed to function
+    mylog.info("Calling function for creating pytorch compatible dataset.")
+    mydataset = CDS(ticker=config['ticker'],
+                    split=config['pars-data']['data-split'],
+                    seq_len=config['pars-data']['size-history'],)
 
 
 
--- src/func_create_dataset.py
+++ src/func_create_dataset.py
@@ -1,5 +1,44 @@
+from email.encoders import encode_quopri
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 import yfinance as yf
+import inspect
+import logging
+import datetime
 
-def create_dataset():
+mylog = logging.getLogger(__name__)
+
+def create_dataset(ticker:str='DB1.DE',
+                   split:tuple[int]=(80, 15, 5),
+                   seq_len:int=5
+                   )->dict:
+    if sum(split) != 100:
+        mylog.error(f"Invalid split values (train, val, test) = {split}")
+        raise ValueError('split must add up to 100')
+    mylog.info(f"Called function: {inspect.stack()[0].function}")
+    ticker = yf.Ticker(ticker)
+    data_raw = ticker.history(period='max')
+    data_raw = data_raw.dropna()
+    mylog.info(f"Raw data shape after dropping NaN rows: {data_raw.shape}")
+    stamps = data_raw.index
+    mylog.info(f"Data covering {stamps[0]} to {stamps[-1]} with {len(stamps)} rows")
+    # preprocessing data -------------------------------------------------------------
+    # split vals for train and val --> test follows from them
+    index_split = np.array([split[0], split[0]+split[1]])/100 * len(data_raw)
+
+    # cutting down dataframes to five features wanted
+    # columns stored in dataframe in  below order and from earliest [first entry]
+    # to latest date [last entry]
+    keys = ['Open', 'High', 'Low', 'Close', 'Volume']
+    targets = data_raw[keys[0]].values
+    data_train = data_raw[keys][:int(index_split[0])].values
+    data_vali = data_raw[keys][int(index_split[0]):int(index_split[1])].values
+    data_test = data_raw[keys][int(index_split[1]):].values
+
+
+    print(data_test)
+
+
+
 
     return 0
